b300cd17e3edf913f9a065e62cc20890108a408f
Kauthar Waggie <kautharwaggie1@gmail.com>
2024-10-26 15:55:02 +0200

    Added unresolved scenarios

--- scripts.js
+++ scripts.js
@@ -5,5 +5,28 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  //added Math floor method this returns only the integer
+  result.innerText = Math.floor(dividend / divider);
+  
+  try{
+    if (!dividend || !divider) {
+      result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+      return;
+    }
+
+    if (divider == 0) {
+      result.innerText = "Division not performed. Invalid number provided. Try again.";
+      console.error("Error: Division by zero attempted. Call stack:", new Error().stack);
+      return;
+    }
+
+    if (isNaN(dividend) || isNaN(divider)) {
+     throw new Error("Non-numeric input provided. Critical failure.");
+    }
+
+} catch (error) {
+  // This displays critical error message and log error with call stack
+  document.body.innerHTML = "<h1>Something critical went wrong. Please reload the page.</h1>";
+  console.error("Critical Error:", error.stack);
+}
 });
